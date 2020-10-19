60b4b50ce164c790b22fc3abc81d3bd8c9bfad3b
Schlanger <leoschlanger@gmail.com>
2020-10-19 19:30:28 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import withApollo from 'next-with-apollo';
 import { ApolloProvider } from '@apollo/react-hooks';
-import App from 'next/app';
 import ThemeContainer from '../contexts/theme/ThemeContainer';
 import ContextProvider from '../contexts/hooks';
 import { client } from '../libs/apolloClient';
@@ -16,11 +15,6 @@ function MyApp({ Component, pageProps, apollo }): JSX.Element {
     </ApolloProvider>
   );
 }
-
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  return { ...appProps };
-};
 
 export default withApollo(({ initialState }) => {
   client.cache.restore(initialState || {});
